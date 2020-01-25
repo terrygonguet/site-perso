95f1ae57275bfd094dfded83498baf52ba696040
terrygonguet <terry@gonguet.com>
2020-01-25 15:38:45 +0100

    Start Stack Nav

---
+++ src/components/StackNavigation.svelte
@@ -0,0 +1,10 @@
+<script>
+  import StackNavigationList from "./StackNavigationList";
+  import radialNavData, { icons } from "../data/radialNav";
+
+  export let _style = "";
+</script>
+
+<section style={_style}>
+  <StackNavigationList data={radialNavData.children} />
+</section>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,11 @@
 <script>
   import RadialNavigation from "../components/RadialNavigation";
+  import StackNavigation from "../components/StackNavigation";
   import { fly } from "svelte/transition";
   import { cubicInOut } from "svelte/easing";
+
+  let w = 0;
+  $: isSmall = w <= 768;
 </script>
 
 <svelte:head>
@@ -11,7 +15,12 @@
 <main
   class="overflow-hidden flex-center relative"
   in:fly={{ easing: cubicInOut, duration: 200, delay: 200, y: -500 }}
-  out:fly={{ easing: cubicInOut, duration: 200, y: -500 }}>
-  <RadialNavigation />
+  out:fly={{ easing: cubicInOut, duration: 200, y: -500 }}
+  bind:clientWidth={w}>
+  {#if isSmall}
+    <StackNavigation />
+  {:else}
+    <RadialNavigation _style="width: 85vmin;height: 85vmin;" />
+  {/if}
   <a href="credits" class="absolute m-4 bottom-0 text-center a">Credits</a>
 </main>
