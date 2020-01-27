5d188313afc84d415601e25e9546f752e67b057d
terrygonguet <terry@gonguet.com>
2020-01-27 01:22:14 +0100

    Actually we can get not visible blogs with a link

--- src/routes/blog/[slug].json.js
+++ src/routes/blog/[slug].json.js
@@ -10,16 +10,12 @@ export async function get(req, res, next) {
 	const { slug } = req.params
 
 	try {
-		let blogdata = await fs.readFile(`src/blogdata/${slug}.json`)
-		let blog = JSON.parse(blogdata)
-		if (!blog.visible) {
-			throw { code: "ENOENT", message: "Blog post is not visible" }
-		} else {
-			res.setHeader("Content-Type", "application/json")
-			delete blog.visible
-			res.end(JSON.stringify(blog))
-		}
+		let blog = await fs.readFile(`src/blogdata/${slug}.json`)
+		res.setHeader("Content-Type", "application/json")
+		delete blog.visible
+		res.end(blog)
 	} catch (err) {
+		console.error(err)
 		let message = "Something went wrong",
 			code = 500
 		if (err.code == "ENOENT") {
