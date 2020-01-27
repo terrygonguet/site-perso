f2feb3912b91ed78ec3d2ef5a3b62b279b52414f
terrygonguet <terry@gonguet.com>
2020-01-27 01:00:18 +0100

    Some content

--- src/routes/projects/abandonned.svelte
+++ src/routes/projects/abandonned.svelte
@@ -1,11 +1,35 @@
 <script>
   import { fade } from "svelte/transition";
   import { cubicInOut } from "svelte/easing";
+  import BlogLink from "../../components/BlogLink";
+
+  let blogs = [
+    {
+      title: "Super 3D Retro",
+      slug: "super-3d-retro",
+      date: "2017-05-12",
+    }
+  ]
 </script>
 
 <main
-  class="overflow-hidden flex-center"
+  class="overflow-hidden flex justify-center items-stretch md:items-center flex-col"
   in:fade={{ easing: cubicInOut, duration: 200, delay: 200 }}
   out:fade={{ easing: cubicInOut, duration: 200 }}>
-  <a href="." class="back">Back</a>
+  <div class="m-4 md:w-2/3 max-h-full overflow-auto text-justify">
+    <p class="my-4">
+      These are projects I started (and stopped) what feels like an age ago, some are just
+      unfinished, some are broken and some only exist in Github and my memories. The fact that
+      I feel like I was a different person when I made these probably tells me that I've done
+      some growing up in between.
+    </p>
+    <p class="my-4">
+      I like to think that I could make them now for a fraction of the time and effort but I
+      haven't tried yet. I made them into blog posts for linkability and organisation.
+    </p>
+    {#each blogs as blog}
+      <BlogLink {...blog} />
+    {/each}
+    <a href="." class="back">Back</a>
+  </div>
 </main>
