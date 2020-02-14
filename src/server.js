38938df59fb8fcc008118ab9e4fc9ddd9b910802
terrygonguet <terry@gonguet.com>
2020-02-14 03:09:20 +0100

    We are LIVE

--- src/server.js
+++ src/server.js
@@ -7,12 +7,11 @@ const { PORT, NODE_ENV } = process.env
 const dev = NODE_ENV === "development"
 
 polka() // You can also use Express
-  .use(
-    "next",
-    compression({ threshold: 0 }),
-    sirv("static", { dev }),
-    sapper.middleware(),
-  )
-  .listen(PORT, err => {
-    if (err) console.log("error", err)
-  })
+	.use(
+		compression({ threshold: 0 }),
+		sirv("static", { dev }),
+		sapper.middleware()
+	)
+	.listen(PORT, err => {
+		if (err) console.log("error", err)
+	})
