e2c179bf59d2964f22991b3b4a2105bc750400e7
terrygonguet <terry@gonguet.com>
2020-02-24 20:56:03 +0100

    Less jank with bloglink

--- src/components/BlogLink.svelte
+++ src/components/BlogLink.svelte
@@ -1,35 +1,41 @@
 <script>
   import { dateFormatter } from "../tools"
+  import { slide } from "svelte/transition";
+  import { onMount } from "svelte";
 
   export let title = "Missing title"
   export let date = "0001-01-01"
   export let slug = "missing-slug"
   export let displayDate = false
 
-  let w
-  $: dateText = displayDate || dateFormatter.format(new Date(date)) 
+  let w, showDate = false
+  $: dateText = displayDate || dateFormatter.format(new Date(date))
+
+  onMount(() => setTimeout(() => (showDate = true), 50))
 </script>
 
 <style>
   a {
     background: var(--radial-bg-dark);
     display: grid;
-    grid-template-columns: auto;
-    grid-template-areas: "title" "date";
-    transition: background-color 0.4s;
+    transition: all 0.4s;
+    grid-template-columns: 1fr auto;
+    grid-template-areas: "title date";
   }
 
   a:hover {
     background: var(--radial-bg-light);
   }
 
-  .big {
-    grid-template-columns: 1fr auto;
-    grid-template-areas: "title date";
+  .small {
+    grid-template-columns: auto;
+    grid-template-areas: "title" "date";
   }
 </style>
 
-<a href="blog/{slug}" class="p-4 m-2 items-center text-left" bind:clientWidth={w} class:big={w > 700} rel="prefetch">
+<a href="blog/{slug}" class="p-4 m-2 items-center text-left" bind:clientWidth={w} class:small={w < 700} rel="prefetch">
   <span class="text-xl" style="grid-area:title">{title}</span>
-  <span class="text-base text-accent-light" style="grid-area:date">{dateText}</span>
+  {#if showDate}
+    <span class="text-base text-accent-light" style="grid-area:date" transition:slide>{dateText}</span>
+  {/if}
 </a>
