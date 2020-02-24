6c91defaf3be87fd8a5758d7ad6c6635b7e4cc26
terrygonguet <terry@gonguet.com>
2020-02-24 20:55:34 +0100

    Layout simplification

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,14 +2,4 @@
   export let segment;
 </script>
 
-<style>
-  /* shimport css */
-  div {
-    display: grid;
-    grid-template-rows: 100%;
-  }
-</style>
-
-<div class="h-full bg-dark text-white text-lg">
-  <slot />
-</div>
+<slot />
