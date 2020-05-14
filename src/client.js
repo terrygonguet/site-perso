4fedacbfd386af40fa051db55dab47b81a73f6b7
terrygonguet <terry@gonguet.com>
2020-05-14 06:49:57 +0200

    Don't need that anymore

--- src/client.js
+++ src/client.js
@@ -1,19 +1,5 @@
 import * as sapper from "@sapper/app"
 
 sapper.start({
-  target: document.querySelector("#sapper"),
+	target: document.querySelector("#sapper")
 })
-
-// force accessibility
-if (process.env.NODE_ENV == "development") {
-  function disableMouse() {
-    let style = document.createElement("style")
-    style.innerText = "* { cursor: none !important }"
-    document.head.append(style)
-  }
-
-  document.addEventListener(
-    "keydown",
-    e => e.key == "m" && e.altKey && disableMouse(),
-  )
-}
