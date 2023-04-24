edcd57e6a5179f8532eb4c47195bcc47d5fbb0e5
terrygonguet <terry@gonguet.com>
2023-04-24 10:18:38 +0200

    feat: :sparkles: Add global component Button

---
+++ src/lib/globals/Button.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	export let appearance: "button" | "anchor" = "button"
+</script>
+
+<button data-appearance={appearance} {...$$props} on:click><slot /></button>
+
+<style lang="postcss">
+	button {
+		@apply transition-colors;
+	}
+
+	button[data-appearance="button"] {
+		@apply bg-orange-600 text-white px-2 py-1 text-lg cursor-pointer;
+	}
+	button[data-appearance="button"]:hover {
+		@apply bg-orange-500;
+	}
+	:is(.dark button[data-appearance="button"]) {
+		@apply bg-amber-600;
+	}
+	:is(.dark button[data-appearance="button"]:hover) {
+		@apply bg-amber-500;
+	}
+
+	button[data-appearance="anchor"] {
+		@apply text-orange-600;
+	}
+	button[data-appearance="anchor"]:hover {
+		@apply text-orange-500 underline;
+	}
+	:is(.dark button[data-appearance="anchor"]) {
+		@apply text-amber-600;
+	}
+	:is(.dark button[data-appearance="anchor"]:hover) {
+		@apply text-amber-500;
+	}
+
+	@media print {
+		button[data-appearance="button"] {
+			@apply bg-stone-700;
+		}
+		button[data-appearance="anchor"] {
+			@apply !text-stone-700 underline;
+		}
+	}
+</style>
--- src/lib/globals/types.d.ts
+++ src/lib/globals/types.d.ts
@@ -1,4 +1,5 @@
 declare const A: typeof import("./A.svelte")["default"]
+declare const Button: typeof import("./Button.svelte")["default"]
 declare const LocalizedParagraph: typeof import("./LocalizedParagraph.svelte")["default"]
 declare const LightBox: typeof import("./LightBox.svelte")["default"]
 
