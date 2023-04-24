68ff87aad74b16f3e915ed46d651d24ec5ede29b
terrygonguet <terry@gonguet.com>
2023-04-24 09:18:49 +0200

    feat: :sparkles: Add loading spinner to long page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,19 @@
-<script>
+<script lang="ts">
 	import "../app.postcss"
 	import { initI18n } from "$i18n"
 	import { initDarkMode } from "$lib/darkMode"
+	import { fade } from "svelte/transition"
+	import Spinner from "~icons/svg-spinners/3-dots-move"
+	import { showLoadingSpinner } from "$lib/stores"
 
 	initI18n()
 	initDarkMode()
 </script>
 
-<slot />
+{#if $showLoadingSpinner}
+	<main class="grid place-items-center" transition:fade={{ duration: 200 }}>
+		<Spinner class="h-16 w-16 text-orange-500 dark:text-amber-600" />
+	</main>
+{:else}
+	<slot />
+{/if}
