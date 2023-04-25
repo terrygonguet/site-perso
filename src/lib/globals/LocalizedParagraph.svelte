4326518c3063ebe8a947d9a573072d9ae004a14c
terrygonguet <terry@gonguet.com>
2023-04-25 11:10:34 +0200

    feat: :sparkles: Rewrite & improve LocalizedParagraph

--- src/lib/globals/LocalizedParagraph.svelte
+++ src/lib/globals/LocalizedParagraph.svelte
@@ -1,25 +1,55 @@
 <script lang="ts">
 	import type { LocalizedString } from "typesafe-i18n"
+	import Printer from "~icons/ri/printer-line"
 
 	type Text = () => LocalizedString
 	type Link = { html: Text; href: Text }
+	type Button = { html: Text; button: Text }
+	type Fragment = Text | Link | Button
 
-	export let fragments: { [i: number]: Text | Link } = {}
+	type Block =
+		| { type: "html"; html: string }
+		| { type: "link"; html: string; href: string }
+		| { type: "button"; appearance: "button" | "anchor"; html: string }
+		| { type: "print"; html: string }
+
+	export let fragments: { [i: number]: Fragment }
 	export let as = "p"
 
 	$: arr = Object.values(fragments)
+	$: blocks = arr.flatMap(fragment2block)
 
-	function isLink(fragment: Text | Link): fragment is Link {
-		return "href" in fragment && "html" in fragment
+	function fragment2block(fragment: Fragment): Block[] {
+		if ("href" in fragment)
+			return [{ type: "link", html: fragment.html(), href: fragment.href() }]
+		if ("button" in fragment) {
+			const button = fragment.button()
+			console.log(button)
+			switch (button) {
+				case "print":
+					return [{ type: "print", html: fragment.html() }]
+				case "anchor":
+					return [{ type: "button", html: fragment.html(), appearance: "anchor" }]
+				default:
+					return [{ type: "button", html: fragment.html(), appearance: "button" }]
+			}
+		}
+		return [{ type: "html", html: fragment() }]
 	}
 </script>
 
-<svelte:element this={as}>
-	{#each arr as fragment}
-		{#if isLink(fragment)}
-			<A href={fragment.href()}>{@html fragment.html()}</A>
-		{:else}
-			{@html fragment()}
+<svelte:element this={as} {...$$props}>
+	{#each blocks as block}
+		{#if block.type == "link"}
+			<A href={block.href}>{@html block.html}</A>
+		{:else if block.type == "button"}
+			<Button on:click appearance={block.appearance}>{@html block.html}</Button>
+		{:else if block.type == "print"}
+			<Button on:click appearance="anchor" on:click={() => print()}
+				><Printer /> {@html block.html}</Button
+			>
+		{:else if block.type == "html"}
+			{@html block.html}
 		{/if}
 	{/each}
 </svelte:element>
