edd595d6be1e7530a5f04e340580a1a779ab8e99
Terry Gonguet <terry@gonguet.com>
2026-05-16 06:06:40 +0200

    Update svelte-i18n for annotations

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -3,6 +3,15 @@ import type { I18NAutocomplete } from "@apptypes"
 import { i18nFetchAll, i18nFetchCategory } from "$lib/i18n.remote"
 
 export const load = async ({ data }) => {
-	const i18n = new SvelteI18N<I18NAutocomplete>({ ...data, fetchAll: i18nFetchAll, fetchCategory: i18nFetchCategory })
+	const i18n = new SvelteI18N<I18NAutocomplete>({
+		...data,
+		fetchAll: i18nFetchAll,
+		fetchCategory: i18nFetchCategory,
+		annotations: {
+			link(content, args) {
+				return `<a href="${args}" ${args?.startsWith("https:") ? 'target="_blank"' : ""} class="anchor">${content}</a>`
+			},
+		},
+	})
 	return { i18n, t: i18n.t.bind(i18n), c: i18n.c.bind(i18n) }
 }
